d9e65ef0a24604d742e1957a4c0ce56af0e2fadc
Negar Shilanan <negarshilanan@gmail.com>
2026-09-17 17:57:32 +0200

    Skapa initial instansiering av kurser och studenter i Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Skapar en kurs med max 2 platser
+        Course csharp = new Course("C# OOP", 2);
+
+        // Skapar 3 studenter
+        Student s1 = new Student("Negar");
+        Student s2 = new Student("Anna");
+        Student s3 = new Student("Erik");
+
+        
