da4e7d4c19c24aff8b77a551d7fc4ae6b88dde79
Negar Shilanan <negarshilanan@gmail.com>
2026-09-17 14:35:25 +0200

    implementera student-Klass med Join, Leave och schedule"

--- Student.cs
+++ Student.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 public class Student
@@ -9,5 +10,32 @@ public class Student
     {
         Name = name;
         Courses = new List<Course>();
+    }
+
+    // Går med i en kurs från studentens sida
+    public void Join(Course course)
+    {
+        course.Enroll(this); // Anropar kursens Enroll-metod
+    }
+
+    // Lämnar en kurs från studentens sida
+    public void Leave(Course course)
+    {
+        course.Remove(this); // Anropar kursens Remove-metod
+    }
+
+    // Skriver ut studentens schema
+    public void Schedule()
+    {
+        Console.WriteLine($"Schema för {Name}:");
+        foreach (Course c in Courses)
+        {
+            Console.WriteLine(c.Name);
+        }
+    }
+
+    public override string ToString()
+    {
+        return Name;
     }
 }
